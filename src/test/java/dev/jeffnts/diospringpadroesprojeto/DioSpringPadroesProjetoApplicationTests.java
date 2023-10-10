c344e28e90495b32d349faee13b5d85b9ad09c4b
Jefferson Nascimento <jf.nts10@gmail.com>
2023-10-10 18:26:07 -0300

    initial commit with spring

---
+++ src/test/java/dev/jeffnts/diospringpadroesprojeto/DioSpringPadroesProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeffnts.diospringpadroesprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringPadroesProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
